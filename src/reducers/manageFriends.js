64e0d1c8c94603323e3606a56fc29ddda9d6c1d8
HMoreira2020 <moreiraheather@gmail.com>
2020-12-06 14:15:41 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+          return { ...state, 
+                friends: [
+                ...state.friends, 
+                action.friend
+            ]
+        }
+        case "REMOVE_FRIEND":
+            const removeIndex = state.friends.map(friend => friend.id).indexOf(action.id)
+            return {
+                ...state,
+                friends: [
+                    ...state.friends.slice(0, removeIndex),
+                    ...state.friends.slice(removeIndex + 1)
+                ]
+            }
+            
+        default:
+          return state;
+      }
 }
