6f24e8e38240f7cc64ed1f5b2964b114a03e7e23
Shamel Lakin <christlife4u@gmail.com>
2020-12-14 17:41:55 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,10 +1,11 @@
-export function managePresents(state, action){
-    switch(action.type) {
-        case 'INCREASE_COUNT':
-            return {count: state.count + 1}
-            case 'DECREASE_COUNT':
-                return { count: state.count - 1}
-                default:
-                    return state;
+export function managePresents(state, action) {
+    state = {
+      numberOfPresents: 0
+    }
+    switch (action.type) {
+         case "INCREASE":
+              return { numberOfPresents: state.numberOfPresents + 1 }
+         default:
+              return state
     }
 }
