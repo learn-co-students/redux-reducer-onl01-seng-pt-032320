4c309826d46140290a7d2049737c5f236362594e
Shamel Lakin <christlife4u@gmail.com>
2020-12-14 17:12:43 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return {friend: name.count + 1}
+            case "REMOVE_FRIEND":
+                return {count: state.count - 1}
+                default:
+                    return state;
+    }
 }
