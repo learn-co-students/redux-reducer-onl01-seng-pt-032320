1e5db9751175ae2f7d4d71ec880bfaf0b9266e4e
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-21 05:28:41 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
+// state = {friends: []}
+// action = {type: "ADD_FRIEND", friend: {name: "Chrome Boi", homewtown: "NYC", id: 1}}
+// action = {type: "REMOVE_FRIEND", id: 1}
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+    let addFriends = []; Object.assign(addFriends, state.friends); addFriends.push(action.friend);
+    return {friends: addFriends}
+    case 'REMOVE_FRIEND':
+    return {friends: state.friends.filter(friend => friend.id !== action.id) }
+    default:
+    return state;
+  }
 }
+
