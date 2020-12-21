4c78f0007261ca37dec0524972f5f0aba0d7e939
nabilhayet <nabil4457@yahoo.com>
2020-12-20 21:37:32 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+        let newFriend = {...friends, name: 'Chrome Boi', hometown: 'NYC', id: 1}
+          return newFriend
+        case 'REMOVE_FRIEND':
+          return 
+        default:
+          return state;
+      }
+
 }
+
+let state = { friends: [] }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+    switch (action.type) {
+        case 'INCREASE':
+          return {numberofPresents: state.numberofPresents + 1}
+        default:
+          return state;
+      }
+
 }
+
+let state = { numberofPresents: 0 }
